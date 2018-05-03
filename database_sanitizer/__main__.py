994504c691ee5276188c76562b465661236f4728
Rauli Laine <rauli.laine@anders.fi>
2018-05-03 15:19:15 +0300

    Fix the main entry point of the package
    Main entry point of the package does not work properly, because it's
    expected to be always callable. Convert contents of `__main__.py` into a
    function and point into that function in `setup.cfg` to fix the issue.

--- database_sanitizer/__main__.py
+++ database_sanitizer/__main__.py
@@ -9,48 +9,56 @@ from .config import Configuration
 from .dump import run
 
 
-parser = argparse.ArgumentParser(
-    description="Sanitizes contents of databases.",
-)
-parser.add_argument(
-    "--config",
-    "-c",
-    nargs=1,
-    type=str,
-    dest="config",
-    help="Path to the sanitizer configuration file.",
-)
-parser.add_argument(
-    "--output",
-    "-o",
-    nargs=1,
-    type=str,
-    dest="output",
-    help=(
-        "Path to the file where the sanitized database will be written into. "
-        "If omitted, standard output will be used instead."
-    ),
-)
-parser.add_argument(
-    "url",
-    help="Database URL to which to connect into and sanitize contents.",
-)
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
 
-args = parser.parse_args()
-output = sys.stdout
-config = None
-
-if args.config:
-    config = Configuration.from_file(args.config[0])
-if args.output:
-    output = open(args.output[0], "w")
-
-try:
-    run(
-        url=args.url,
-        output=output,
-        config=config,
+    parser = argparse.ArgumentParser(
+        description="Sanitizes contents of databases.",
     )
-finally:
+    parser.add_argument(
+        "--config",
+        "-c",
+        nargs=1,
+        type=str,
+        dest="config",
+        help="Path to the sanitizer configuration file.",
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        nargs=1,
+        type=str,
+        dest="output",
+        help=(
+            "Path to the file where the sanitized database will be written into. "
+            "If omitted, standard output will be used instead."
+        ),
+    )
+    parser.add_argument(
+        "url",
+        help="Database URL to which to connect into and sanitize contents.",
+    )
+
+    args = parser.parse_args(args=args)
+    output = sys.stdout
+    config = None
+
+    if args.config:
+        config = Configuration.from_file(args.config[0])
     if args.output:
-        output.close()
+        output = open(args.output[0], "w")
+
+    try:
+        run(
+            url=args.url,
+            output=output,
+            config=config,
+        )
+    finally:
+        if args.output:
+            output.close()
+
+
+if __name__ == "__main__":
+    main()
