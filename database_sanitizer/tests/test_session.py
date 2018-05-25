5df2a24bc713c3d51c8fa5d60ef8e15067402041
Tuomas Suutari <tuomas.suutari@anders.fi>
2018-05-25 15:58:56 +0300

    Add sanitation sesssion API
    Sanitation session allows having a state within a single sanitation
    process.
    
    One important thing stored to the session is a secret key which is
    generated to a new random value for each sanitation session, but it
    stays constant during the whole sanitation process. Its value is never
    revealed, so that it is possible to generate such one way hashes with
    it, that should not be redoable afterwards. I.e. during the sanitation
    session it's possible to do ``hash(C) -> H`` for any clear text C, but
    it is not possible to check if H is the hashed value of C after the
    sanitation session has ended.

---
+++ database_sanitizer/tests/test_session.py
@@ -0,0 +1,34 @@
+from database_sanitizer import session
+
+session.reset(b'not-so-secret-key')
+
+
+def test_hash_text_to_int():
+    assert session.hash_text_to_int('hello') == 4100462238
+
+
+def test_hash_text_to_ints():
+    assert session.hash_text_to_ints('hello', [4, 8, 16]) == (15, 70, 33129)
+
+
+def test_hash_text():
+    assert session.hash_text('hello') == (
+        'f468169e17f4dd5d7318bd6099a4e657ceb0a978cddb4f3382be0da7121659bb')
+
+
+def test_hash_bytes():
+    assert session.hash_bytes(b'hello') == (
+        'f468169e17f4dd5d7318bd6099a4e657ceb0a978cddb4f3382be0da7121659bb')
+
+
+def test_get_secret():
+    assert session.get_secret() == b'not-so-secret-key'
+
+
+def test_reset():
+    old_key = session.get_secret()
+    session.reset()
+    new_key = session.get_secret()
+    assert new_key != old_key
+    session.reset(b'not-so-secret-key')
+    assert session.get_secret() == b'not-so-secret-key'
